03ef3a83fb68ca34f7a7b45dbfa51bc37bb29656
longvu <longvu.bat@gmail.com>
2021-03-24 00:06:54 +0700

    first commit

---
+++ app/src/main/java/com/a1_1801040235/modernapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.a1_1801040235.modernapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
